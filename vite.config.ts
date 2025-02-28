d0072dcc9985d0bf8f1f66d3e5ab9717d15e21b5
aofong <84339706@qq.com>
2025-02-28 17:16:13 +0800

    更新deploy.yml

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  base: process.env.PUBLIC_URL || '/',
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   ssr: { noExternal: ['antd-mobile'] },
 });
